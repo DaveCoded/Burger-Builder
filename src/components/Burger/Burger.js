e08ad3de852d66988ff4967b5d0419b787ecfbb7
David Bernhard <davidbernhard@live.com>
2019-09-29 13:43:58 +0100

    check for no ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,16 +4,26 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const Burger = props => {
-  // Turn object of ingredients into an array
-  const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
-    return (
-      [...Array(props.ingredients[igKey])]
-        // Then map through that array, outputting a list of ingredients
-        .map((_, i) => {
-          return <BurgerIngredient key={igKey + i} type={igKey} />;
-        })
-    );
-  });
+  // Map through array of ingredient object's keys
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return (
+        // return an array for each ingredient type
+        [...Array(props.ingredients[igKey])]
+          // then map through that inner array, outputting a component per array item
+          .map((_, i) => {
+            return <BurgerIngredient key={igKey + i} type={igKey} />;
+          })
+      );
+    })
+    // Flatten nested array to linear array to get total ingredients length
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients</p>;
+  }
 
   return (
     <div className={classes.Burger}>
